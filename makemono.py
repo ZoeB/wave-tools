63db45fdc285e5266791a6610647ea428a1fee02
Zoë Blade <zoe@bytenoise.co.uk>
2012-03-31 14:14:44 +0100

    Getting there... quickly stopping to help make lunch!  Currently buggy.

--- makemono.py
+++ makemono.py
@@ -1,5 +1,10 @@
 # Makemono, for Python 3.  By ZoëB, 2012-03-31 - 2012-03-31.
 
+# This converts a stereo .wav file to mono.
+# It's useful if, for instance, you've recorded a synthesiser using a
+# stereo only sound recorder.
+# It works by simply disregarding the right channel entirely.
+
 import sys # For command line arguments
 import wave # For .wav input and output
 
@@ -15,11 +20,24 @@ if (inputFilename[-4:] != '.wav'):
 	print('Please specify a .wav file')
 	exit()
 
+outputFilename = inputFilename[:-4] + '-mono' + '.wav';
+
 try:
 	inputFile = wave.open(inputFilename, 'r')
+	outputFile = wave.open(outputFilename, 'w')
+	outputFile.setnchannels(1)
+	outputFile.setsampwidth(inputFile.getsampwidth())
+	outputFile.setframerate(inputFile.getframerate())
 except:
 	print('Please specify a valid .wav file')
 	exit()
-else:
-	print('Converting', inputFilename, 'to mono')
+
+if (inputFile.getnchannels() != 2):
+	print('Please specify a stereo .wav file')
 	exit()
+
+for sample in range (0, inputFile.getnframes()):
+	outputFile.writeframes(sample)
+
+inputFile.close()
+outputFile.close()
