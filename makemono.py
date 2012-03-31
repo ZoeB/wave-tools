509e58a36807f44bba49b43c5c33343e095b6a10
Zoë Blade <zoe@bytenoise.co.uk>
2012-03-31 15:05:33 +0100

    Clarified comment.

--- makemono.py
+++ makemono.py
@@ -38,7 +38,7 @@ if (inputFile.getnchannels() != 2):
 
 for iteration in range (0, inputFile.getnframes()):
 	datum = inputFile.readframes(1)
-	outputFile.writeframes(datum[0:2]) # This assumes the datum consists of two actual samples, both 16-bit.  The first 16-bit sample (parts 0 and 1) is output; the second 16-bit sample (parts 2 and 3) isn't.  The first sample is the left channel, the second the right channel.
+	outputFile.writeframes(datum[0:2]) # This assumes the waveform is 16-bit.  It writes the first two bytes (the left channel), ignoring the last two (the right channel).
 
 inputFile.close()
 outputFile.close()
