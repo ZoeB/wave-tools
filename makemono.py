29711a35242f824b24d3510941deddcefcfd66ec
Zoë Blade <zoe@bytenoise.co.uk>
2012-03-31 13:56:16 +0100

    More coding... trying to open the file is currently buggy, for some reason.

--- makemono.py
+++ makemono.py
@@ -3,11 +3,23 @@
 import sys # For command line arguments
 import wave # For .wav input and output
 
-inputFilename = sys.argv[1]
-
-if (len(sys.argv) != 2 or inputFilename[-4:] != '.wav'):
+# Make sure the user has specified exactly one argument...
+if (len(sys.argv) != 2):
 	print('Please specify a single .wav file')
 	exit()
 
-print('Converting', inputFilename, 'to mono')
-exit()
+# ...and that argument is a .wav file.
+inputFilename = sys.argv[1]
+
+if (inputFilename[-4:] != '.wav'):
+	print('Please specify a .wav file')
+	exit()
+
+try:
+	inputFile = wave.open(inputFilename, r)
+except:
+	print('Please specify a valid .wav file')
+	exit()
+else:
+	print('Converting', inputFilename, 'to mono')
+	exit()
