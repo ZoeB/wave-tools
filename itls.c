63602849e9cd60b7573a7b782c65832b11f4608c
Zoë Blade <zoe@bytenoise.co.uk>
2014-01-08 16:25:57 +0000

    List modules and instruments as well as samples
    The Impulse Tracker sample describer is now the Impulse Tracker
    lister, listing modules, instruments and samples.
    
    Squashed commit of the following:
    
    commit 493d4446f10f31df9ad1133c330a2bc3e4ee51d6
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Wed Jan 8 16:22:49 2014 +0000
    
        List modules too
    
        Ditto.
    
    commit af77ff75a0e0cd547670cf3470194426bb8b7519
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Wed Jan 8 16:09:49 2014 +0000
    
        List instruments too
    
        Although not their constituent samples yet...
    
    commit 27b8ee75c2b78ec7412b1218dada2db76675ab0a
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Wed Jan 8 16:02:54 2014 +0000
    
        Refactor code slightly
    
    commit 6e0917116ee1dc3b3220deb92cc220e177ab638d
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Wed Jan 8 15:56:37 2014 +0000
    
        Empty commit; note
    
        Ah, this is indeed working (I'm not sure for how long); I was testing
        it on the wrong directory, not enough subdirectories deep to actually
        contain the .ITS files...  XD
    
    commit 16baba41c08aa3cb7f613bb8a2eef50a49670bdb
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Wed Jan 8 15:18:54 2014 +0000
    
        Optimise code
    
    commit 45a50efacfd3ed2c7cce80057ae88be1f0abf3a1
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Wed Jan 8 15:18:07 2014 +0000
    
        Remove unused line
    
        That was part of the cat example...
    
    commit 8b3a77c8ea86e6223101beaa9a5b4ed82572cd1e
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Wed Jan 8 15:17:46 2014 +0000
    
        Split out methods
    
        Although it doesn't work yet...
    
    commit 98bbffc3d6f19bb0e1cecd3a3352f4fdc95080be
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Wed Jan 8 15:08:52 2014 +0000
    
        Rename variables more verbosely
    
    commit 396d1f03df551c12250e388290684b6a387ae914
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Wed Jan 8 15:05:49 2014 +0000
    
        Fix counting character offset
    
    commit 10d0fa8326bcc1422c62fc0d071b6e3c6fbe285b
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Wed Jan 8 15:03:27 2014 +0000
    
        Improve file detection
    
    commit e9c36327dc29f07acb3e6a1a35c798e9e9013a8a
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Wed Jan 8 14:59:41 2014 +0000
    
        Continue preparing for extending
    
    commit f905671ed32419b9d837c2467756f0abf3733db6
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Wed Jan 8 14:55:14 2014 +0000
    
        Get ready for featurecreep!

---
+++ itls.c
@@ -0,0 +1,164 @@
+/* Impulse Tracker file lister, by Zoe Blade */
+
+#include <stdio.h>
+#include "itls.h"
+
+void describeFile(FILE *inputFilePointer, FILE *outputFilePointer) {
+	/* If the input doesn't begin with "IMP", it's not an Impulse Tracker
+	   file.  Quit. */
+
+	if (getc(inputFilePointer) != 'I')
+		return;
+
+	if (getc(inputFilePointer) != 'M')
+		return;
+
+	if (getc(inputFilePointer) != 'P')
+		return;
+
+	switch (getc(inputFilePointer)) {
+	case 'I': /* Instrument */
+		describeInstrument(inputFilePointer, outputFilePointer);
+		break;
+
+	case 'M': /* Module (song) */
+		describeModule(inputFilePointer, outputFilePointer);
+		break;
+
+	case 'S': /* Sample */
+		describeSample(inputFilePointer, outputFilePointer);
+		break;
+
+	default:
+		return;
+	}
+}
+
+void describeInstrument(FILE *inputFilePointer, FILE *outputFilePointer) {
+	/* TODO: name its samples too, one S-line each */
+	int characterNumber;
+	int character;
+
+	putc('I', outputFilePointer);
+	putc(' ', outputFilePointer);
+
+	for (characterNumber = 4; characterNumber < 57; characterNumber++) {
+		character = getc(inputFilePointer);
+
+		if (character == EOF) {
+			return;
+		}
+
+		if ((characterNumber > 3 && characterNumber < 16) || (characterNumber > 31)) {
+			/* Only print out the filename and instrument name, nothing in between. */
+			if (character == '\0') {
+				/* Pad filenames and instrument names with spaces, not NULLs. */
+				putc(' ', outputFilePointer);
+			} else {
+				putc(character, outputFilePointer);
+			}
+		} else if (characterNumber == 16) {
+				/* Print a gap between the filename and instrument name. */
+				putc(' ', outputFilePointer);
+		}
+	}
+
+	/* We've finished successfully printing out an instrument's filename
+	   and instrument name.  Print a newline and return. */
+
+	putc('\n', outputFilePointer);
+	return;
+}
+
+void describeModule(FILE *inputFilePointer, FILE *outputFilePointer) {
+	/* TODO: name its instruments and samples too, one I- and S-line each */
+	int characterNumber;
+	int character;
+	int i;
+
+	putc('M', outputFilePointer);
+
+	for (i = 0; i < 14; i++) {
+		putc(' ', outputFilePointer); /* Modules have no internally
+		                                 stored filenames */
+	}
+
+	for (characterNumber = 4; characterNumber < 30; characterNumber++) {
+		character = getc(inputFilePointer);
+
+		if (character == EOF) {
+			return;
+		}
+
+		if (character == '\0') {
+			/* Pad module names with spaces, not NULLs. */
+			putc(' ', outputFilePointer);
+		} else {
+			putc(character, outputFilePointer);
+		}
+	}
+
+	/* We've finished successfully printing out a sample's filename
+	   and sample name.  Print a newline and return. */
+
+	putc('\n', outputFilePointer);
+	return;
+}
+
+void describeSample(FILE *inputFilePointer, FILE *outputFilePointer) {
+	int characterNumber;
+	int character;
+
+	putc('S', outputFilePointer);
+	putc(' ', outputFilePointer);
+
+	for (characterNumber = 4; characterNumber < 45; characterNumber++) {
+		character = getc(inputFilePointer);
+
+		if (character == EOF) {
+			return;
+		}
+
+		if ((characterNumber > 3 && characterNumber < 16) || (characterNumber > 19)) {
+			/* Only print out the filename and sample name, nothing in between. */
+			if (character == '\0') {
+				/* Pad filenames and sample names with spaces, not NULLs. */
+				putc(' ', outputFilePointer);
+			} else {
+				putc(character, outputFilePointer);
+			}
+		} else if (characterNumber == 16) {
+				/* Print a gap between the filename and sample name. */
+				putc(' ', outputFilePointer);
+		}
+	}
+
+	/* We've finished successfully printing out a sample's filename
+	   and sample name.  Print a newline and return. */
+
+	putc('\n', outputFilePointer);
+	return;
+}
+
+/* This is mostly ripping off K&R's ANSI C, page 162, "Cat". */
+
+int main(int argc, char *argv[]) {
+	FILE *filePointer;
+
+	if (argc == 1) {
+		describeFile(stdin, stdout);
+	} else {
+		while (--argc > 0) {
+			filePointer = fopen(*++argv, "r");
+
+			if (filePointer == NULL) {
+				continue;
+			}
+
+			describeFile(filePointer, stdout);
+			fclose(filePointer);
+		}
+	}
+
+	return 0;
+}
---
+++ itls.h
@@ -0,0 +1,10 @@
+#ifndef ITLS_H
+#define ITLS_H
+
+void describeFile();
+void describeInstrument();
+void describeModule();
+void describeSample();
+int main();
+
+#endif /* ITLS_H */
