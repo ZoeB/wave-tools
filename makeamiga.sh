df531f264799c95d3835d2d7e9c5fe36cef8468d
Zoë Blade <zoe@bytenoise.co.uk>
2015-03-18 19:20:12 +0000

    Normalise file first

--- makeamiga.sh
+++ makeamiga.sh
@@ -2,8 +2,7 @@
 
 for file in *.wav
 do
-	sox $file -b 8 --rate 8363 ${file%.wav}-8363hz.wav
+	sox --norm $file -b 8 --rate 8363 ${file%.wav}-8363hz.wav
 	rm $file
 	mv ${file%.wav}-8363hz.wav $file
 done
-
