c24dd4d54d1b6fd1826fe7db52dd44b12915b239
Zoë Blade <zoe@bytenoise.co.uk>
2015-03-19 16:06:36 +0000

    Make scripts complex-filename compatible

--- makecd.sh
+++ makecd.sh
@@ -2,7 +2,7 @@
 
 for file in *.wav
 do
-	sox --norm $file -b 16 --rate 44100 ${file%.wav}-cd.wav
-	rm $file
-	mv ${file%.wav}-cd.wav $file
+	sox --norm "${file}" -b 16 --rate 44100 "${file%.wav}-cd.wav"
+	rm "${file}"
+	mv "${file%.wav}-cd.wav" "${file}"
 done
