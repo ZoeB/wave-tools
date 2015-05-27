899add496e428c0ce3c38a66c4df0b3dd736c371
Zoë Blade <zoe@bytenoise.co.uk>
2015-05-27 14:23:02 +0100

    Rename variable more usefully

--- pitches.c
+++ pitches.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-	int i;
+	int pitch;
 	int octave;
 	float frequency;
 
-	for (i = 1; i < 89; i++) {
-		octave = (i + 8) / 12;
-		frequency = pow(2, (i - 49) / 12.0) * 440;
-		printf("%d\t%d\t%8.3f\n", i, octave, frequency);
+	for (pitch = 1; pitch < 89; pitch++) {
+		octave = (pitch + 8) / 12;
+		frequency = pow(2, (pitch - 49) / 12.0) * 440;
+		printf("%d\t%d\t%8.3f\n", pitch, octave, frequency);
 	}
 
 	return 0;
