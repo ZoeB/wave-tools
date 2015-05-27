9868d60a5a4f0adac1745c4b7d9707d0241256a2
Zoë Blade <zoe@bytenoise.co.uk>
2015-05-27 14:22:14 +0100

    Print octaves too

--- pitches.c
+++ pitches.c
@@ -3,11 +3,13 @@
 
 int main(int argc, char *argv[]) {
 	int i;
+	int octave;
 	float frequency;
 
 	for (i = 1; i < 89; i++) {
+		octave = (i + 8) / 12;
 		frequency = pow(2, (i - 49) / 12.0) * 440;
-		printf("%d\t%8.3f\n", i, frequency);
+		printf("%d\t%d\t%8.3f\n", i, octave, frequency);
 	}
 
 	return 0;
