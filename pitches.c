aad3fff42a2382de253612a79456ca4e3bba4be4
Zoë Blade <zoe@bytenoise.co.uk>
2015-05-27 14:33:10 +0100

    Print the actual notes too

--- pitches.c
+++ pitches.c
@@ -3,13 +3,17 @@
 
 int main(int argc, char *argv[]) {
 	int pitch;
+	int modulo;
+	char noteLetter[12] = "CCDDEFFGGAAB";
+	char noteIntonation[12] = "-#-#--#-#-#-";
 	int octave;
 	float frequency;
 
 	for (pitch = 1; pitch < 89; pitch++) {
 		octave = (pitch + 8) / 12;
+		modulo = (pitch + 8) % 12;
 		frequency = pow(2, (pitch - 49) / 12.0) * 440;
-		printf("%d\t%d\t%8.3f\n", pitch, octave, frequency);
+		printf("%d\t%c%c%d\t%8.3f\n", pitch, noteLetter[modulo], noteIntonation[modulo], octave, frequency);
 	}
 
 	return 0;
