cf9e9716718d25b5533f5b940708b8f9bf217639
Zoë Blade <zoe@bytenoise.co.uk>
2017-01-06 12:10:41 +0000

    Create ASCII to Baudot-in-MIDI converter
    Squashed commit of the following:
    
    commit 460d0dfa5f9f424386d6dea55bc9c483ece54103
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Fri Jan 6 12:09:34 2017 +0000
    
        Rename script
    
    commit 7197e321694d8aca69634d03f18a4cf931c46c4e
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Fri Jan 6 12:09:10 2017 +0000
    
        Comment more
    
    commit 53ef32b7fca98af554b4fbcf51797dca0a520a6b
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Fri Jan 6 12:03:38 2017 +0000
    
        Allow arbitrary messages
    
    commit f30a8e85e6755df5ef07b1f67c773cda17e42d95
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Fri Jan 6 11:58:18 2017 +0000
    
        Fix other bug
    
        This now looks right! :D
    
    commit e0326f0fe0bd8f901cd11fd98d75463676cb66d4
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Fri Jan 6 11:55:01 2017 +0000
    
        Debug more
    
        It's easier in this order.
    
    commit 7d5afecdf82344c98cb95699c3823eb193ff43e1
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Fri Jan 6 11:53:13 2017 +0000
    
        Start fixing bugs
    
    commit 514147bf3499d3017d60de195a5849760c2cc75e
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Fri Jan 6 11:51:45 2017 +0000
    
        Work more on actual gubbins of code
    
    commit fe0437f7472c6bdf678152e5c10dbe84177a9686
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Fri Jan 6 11:34:11 2017 +0000
    
        Start work on proper MIDI rendering
    
    commit d3d2e6d8af36ffd4e662450588d25f63ef263325
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Fri Jan 6 11:29:35 2017 +0000
    
        Do it properly, using an array
    
    commit a0214944dcb8aaf63b3f3e06a088854c56b378ae
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Thu Jan 5 20:54:07 2017 +0000
    
        Note what to do tomorrow
    
    commit 2e22b3b58a1d3868214ac613a915bb26ba560751
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Thu Jan 5 20:50:49 2017 +0000
    
        Work out Baudot
    
        From
        https://en.wikipedia.org/wiki/Baudot_code#/media/File:International_Telegraph_Alphabet_2.jpg
        .  I should proofread this tomorrow morning.
    
    commit 4450959c005fdfd7858f62d2c88e72626b585367
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Thu Jan 5 20:44:30 2017 +0000
    
        Verify in-list condition
    
    commit 0e918e89bb1f76b174f65804951aca741f2625ae
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Thu Jan 5 20:38:59 2017 +0000
    
        Force uppercase for Baudot
    
        See https://en.wikipedia.org/wiki/Baudot_code#Baudot_code .
    
    commit ffc1ce557d911a05436240bf77f7857bc98a71d3
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Thu Jan 5 20:36:41 2017 +0000
    
        Loop through message...
    
    commit 0531529068c573b584c9fa6e5bcf6b53785576f3
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Thu Jan 5 20:33:46 2017 +0000
    
        Work out good tick length via trial and error
    
        Not the best way. :/
    
    commit 52164a6c0dcad7cddc0f2683abf035bf1d06d977
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Thu Jan 5 20:27:25 2017 +0000
    
        Experiment with timings
    
        Successfully!
    
    commit 22293204d9631d7d993e8f8646661bfb7c5f4b9d
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Thu Jan 5 20:24:52 2017 +0000
    
        Tidy
    
    commit 844e67f8f37f5bae0d5a81b2166f4b161638c815
    Author: Zoë Blade <zoe@bytenoise.co.uk>
    Date:   Thu Jan 5 20:21:47 2017 +0000
    
        Write up example program
    
        From
        https://github.com/vishnubob/python-midi#Building_a_MIDI_File_from_scratch
        .  It works!

---
+++ baudot-in-midi.py
@@ -0,0 +1,96 @@
+# ASCII to Baudot-in-MIDI
+# For Python 2
+# By Zoe Blade
+
+# Converts a specified message to Baudot within a MIDI file, to embed messages in music as rhythmic harmonies
+
+import midi # Installed from https://github.com/vishnubob/python-midi
+import sys # For command line arguments
+
+if len(sys.argv) < 2:
+	print 'Please specify a message to convert'
+	exit()
+
+message = sys.argv[1].upper()
+noteLength = 110
+
+pattern = midi.Pattern()
+track = midi.Track()
+pattern.append(track)
+tick = 0
+
+for letter in message:
+	if letter not in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ ':
+		continue
+
+	elements = [False, False, False, False, False]
+
+	# Using https://en.wikipedia.org/wiki/Baudot_code#/media/File:International_Telegraph_Alphabet_2.jpg as a guide
+	if letter in 'ABDEFJKQSUWXYZ':
+		elements[0] = True
+
+	if letter in 'ACGIJKLPQRUVW':
+		elements[1] = True
+
+	if letter in 'CFHIKMNPQSUVXY ':
+		elements[2] = True
+
+	if letter in 'BCDFGJKMNORVX':
+		elements[3] = True
+
+	if letter in 'BGHLMOPQTVWXYZ':
+		elements[4] = True
+
+	lineToPrint = letter + ' '
+	elementCount = 0
+
+	for element in elements:
+		if element == True:
+			lineToPrint += 'o'
+
+			if elementCount == 0:
+				track.append(midi.NoteOnEvent(tick=0, velocity=100, pitch=midi.E_4))
+			elif elementCount == 1:
+				track.append(midi.NoteOnEvent(tick=0, velocity=100, pitch=midi.C_4))
+			elif elementCount == 2:
+				track.append(midi.NoteOnEvent(tick=0, velocity=100, pitch=midi.G_3))
+			elif elementCount == 3:
+				track.append(midi.NoteOnEvent(tick=0, velocity=100, pitch=midi.E_3))
+			else:
+				track.append(midi.NoteOnEvent(tick=0, velocity=100, pitch=midi.C_3))
+
+		else:
+			lineToPrint += '.'
+
+		elementCount += 1
+
+	movedOnYet = False
+	elementCount = 0
+
+	for element in elements:
+		if element == True:
+			if movedOnYet == True:
+				ticks = 0
+			else:
+				ticks = noteLength
+
+			if elementCount == 0:
+				track.append(midi.NoteOffEvent(tick=ticks, pitch=midi.E_4))
+			elif elementCount == 1:
+				track.append(midi.NoteOffEvent(tick=ticks, pitch=midi.C_4))
+			elif elementCount == 2:
+				track.append(midi.NoteOffEvent(tick=ticks, pitch=midi.G_3))
+			elif elementCount == 3:
+				track.append(midi.NoteOffEvent(tick=ticks, pitch=midi.E_3))
+			else:
+				track.append(midi.NoteOffEvent(tick=ticks, pitch=midi.C_3))
+
+			movedOnYet = True
+
+		elementCount += 1
+
+	print lineToPrint
+
+track.append(midi.EndOfTrackEvent(tick=1))
+midi.write_midifile('baudot.mid', pattern)
+print 'Exported baudot.mid'
