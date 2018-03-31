faf11c7d25221747ffad8393a7f14f4a01b3e915
Zoë Blade <zoe@bytenoise.co.uk>
2018-03-31 12:43:41 +0100

    Tidy output formatting

--- pitches.c
+++ pitches.c
@@ -11,14 +11,14 @@ int main(int argc, char *argv[]) {
 	int octave;
 	float frequency;
 
-	printf("Pitch #\tName\tHz\n");
+	printf("MIDI #\tName\tHz\n");
 	printf("=======\t=======\t========\n\n");
 
 	for (pitch = 1; pitch < 89; pitch++) {
 		octave = (pitch + 8) / 12;
 		modulo = (pitch + 8) % 12;
 		frequency = pow(2, (pitch - 49) / 12.0) * 440;
-		printf("%2d\t%c%c%d\t%8.3f\n", pitch + 20, noteLetter[modulo], noteIntonation[modulo], octave, frequency);
+		printf("%7 d\t%c%c%d\t%8.3f\n", pitch + 20, noteLetter[modulo], noteIntonation[modulo], octave, frequency);
 	}
 
 	return 0;
