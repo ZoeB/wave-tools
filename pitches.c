4f1d93432c12d6d7fd3dbbc7def703f01d22006c
Zoë Blade <zoe@bytenoise.co.uk>
2018-04-03 12:59:22 +0100

    Add 1V/Oct voltage to table
    Note that this is heavily non-standardised:
    https://learningmodular.com/matching-octaves-in-your-modular/
    
    At a glance, 2 V for C4 seems a slight majority winner, plus it's
    what Kenton currently uses, and they make my MIDI to CV converter.
    
    Edit: nope, it's second place in terms of popularity.  I really need
    to add a disclaimer.

--- pitches.c
+++ pitches.c
@@ -9,16 +9,18 @@ int main(int argc, char *argv[]) {
 	char noteLetter[12] = "CCDDEFFGGAAB";
 	char noteIntonation[12] = "-#-#--#-#-#-";
 	int octave;
+	float voltage;
 	float frequency;
 
-	printf("Name\tMIDI #\tHz\n");
-	printf("=======\t=======\t========\n\n");
+	printf("Name\tMIDI #\tVoltage\tHz\n");
+	printf("=======\t=======\t=======\t========\n\n");
 
 	for (pitch = 1; pitch < 89; pitch++) {
 		octave = (pitch + 8) / 12;
 		modulo = (pitch + 8) % 12;
+		voltage = octave - 2 + modulo / 12.0;
 		frequency = pow(2, (pitch - 49) / 12.0) * 440;
-		printf("%c%c%d\t%7d\t%8.2f\n", noteLetter[modulo], noteIntonation[modulo], octave, pitch + 20, frequency);
+		printf("%c%c%d\t%7d\t%5.3f\t%8.2f\n", noteLetter[modulo], noteIntonation[modulo], octave, pitch + 20, voltage, frequency);
 	}
 
 	return 0;
