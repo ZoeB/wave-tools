7654657dbcd4e0bff831a35560cb15f7119e4f8e
Zoë Blade <zoe@bytenoise.co.uk>
2018-04-03 12:44:39 +0100

    Swap columns

--- pitches.c
+++ pitches.c
@@ -11,14 +11,14 @@ int main(int argc, char *argv[]) {
 	int octave;
 	float frequency;
 
-	printf("MIDI #\tName\tHz\n");
+	printf("Name\tMIDI #\tHz\n");
 	printf("=======\t=======\t========\n\n");
 
 	for (pitch = 1; pitch < 89; pitch++) {
 		octave = (pitch + 8) / 12;
 		modulo = (pitch + 8) % 12;
 		frequency = pow(2, (pitch - 49) / 12.0) * 440;
-		printf("%7d\t%c%c%d\t%8.2f\n", pitch + 20, noteLetter[modulo], noteIntonation[modulo], octave, frequency);
+		printf("%c%c%d\t%7d\t%8.2f\n", noteLetter[modulo], noteIntonation[modulo], octave, pitch + 20, frequency);
 	}
 
 	return 0;
