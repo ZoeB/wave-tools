2938d00b2e98b0f655a0d0a2b3d68435da411153
Zoë Blade <zoe@bytenoise.co.uk>
2018-04-03 13:09:23 +0100

    Let's go with the most popular one, then
    C4 = 3V, not 2V.

--- pitches.c
+++ pitches.c
@@ -32,7 +32,7 @@ int main(int argc, char *argv[]) {
 	for (pitch = 1; pitch < 89; pitch++) {
 		octave = (pitch + 8) / 12;
 		modulo = (pitch + 8) % 12;
-		voltage = octave - 2 + modulo / 12.0;
+		voltage = octave - 1 + modulo / 12.0;
 		frequency = pow(2, (pitch - 49) / 12.0) * 440;
 		printf("%c%c%d\t%7d\t%5.3f\t%8.2f\n", noteLetter[modulo], noteIntonation[modulo], octave, pitch + 20, voltage, frequency);
 	}
