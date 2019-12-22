d297415223b41dd4bb97229edca418b64dad7280
Zoë Blade <zoe@bytenoise.co.uk>
2019-12-22 17:13:53 +0000

    Work with files with awkward filenames

--- makemono.sh
+++ makemono.sh
@@ -2,7 +2,7 @@
 
 for file in *.wav
 do
-	sox $file -c 1 ${file%.wav}-mono.wav remix 1
-	rm $file
-	mv ${file%.wav}-mono.wav $file
+	sox "$file" -c 1 "${file%.wav}-mono.wav" remix 1
+	rm "$file"
+	mv "${file%.wav}-mono.wav" "$file"
 done
