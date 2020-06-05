3e5c5b68cdf2223df1328d847e5029744ac2a69e
Zoë Blade <zoe@bytenoise.co.uk>
2020-06-05 18:33:24 +0100

    Cope with fancy filenames

--- make16bit.sh
+++ make16bit.sh
@@ -2,7 +2,7 @@
 
 for file in *.wav
 do
-	sox $file --norm -b 16 ${file%.wav}-16bit.wav
-	rm $file
-	mv ${file%.wav}-16bit.wav $file
+	sox "$file" --norm -b 16 "${file%.wav}-16bit.wav"
+	rm "$file"
+	mv "${file%.wav}-16bit.wav" "$file"
 done
