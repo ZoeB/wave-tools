d1fe70ea8fef88fdd5ad2809980036e242a9d11b
Zoë Blade <zoe@bytenoise.co.uk>
2020-11-26 14:55:31 +0000

    Don't show the reference out-of-range "MIDI note numbers"
    That'll just confuse people.

--- bpm-as-pitch.c
+++ bpm-as-pitch.c
@@ -1,7 +1,5 @@
 /* BPM as pitch tabulator, to tune tempos, for ANSI C, by ZB, 2020 */
 /* Based on pitch tabulator, for ANSI C, by Zoe Blade, 2015, 2018 */
-/* Obviously, these are not real MIDI numbers (which are positive,
-   between 0 and 127 inclusive), just for reference */
 
 #include <math.h>
 #include <stdio.h>
@@ -15,15 +13,15 @@ int main(int argc, char *argv[]) {
 	float frequency;
 	float tempo;
 
-	printf("Name\tMIDI #\tHz\tBPM\n");
-	printf("=======\t=======\t=======\t========\n\n");
+	printf("Name\tHz\tBPM\n");
+	printf("========\t=======\t========\n\n");
 
 	for (pitch = -56; pitch < -32; pitch++) {
 		octave = (pitch + 8) / 12;
 		modulo = (pitch + 8 + 60) % 12; /* Add 60 just to get it in the positive range, to calculate the modulo */
 		frequency = pow(2, (pitch - 49) / 12.0) * 440;
 		tempo = frequency * 60;
-		printf("%c%c%d\t%7d\t%5.3f\t%8.3f\n", noteLetter[modulo], noteIntonation[modulo], octave, pitch + 20, frequency, tempo);
+		printf("%c%c%d\t%5.3f\t%8.3f\n", noteLetter[modulo], noteIntonation[modulo], octave, frequency, tempo);
 	}
 
 	return 0;
