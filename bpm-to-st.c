ffae5b8bfa10037b8ee3ef98923f38273c706306
Zoë Blade <zoe@bytenoise.co.uk>
2024-04-04 15:42:00 +0100

    Tidy terminology and output

--- bpm-to-st.c
+++ bpm-to-st.c
@@ -1,21 +1,18 @@
-/* BPM to semitone converter, for ANSI C, by Zoe Blade, 2024-04-04 */
+/* BPM transposer, for ANSI C, by Zoe Blade, 2024-04-04 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
-void beatsPerMinuteToSemitones(int beatsPerMinute) {
-	float millisecondsPerBeat;
+void transposeBeatsPerMinute(int beatsPerMinute) {
 	int semitones;
 
 	/*
-	 * The old tempo * 2 ^ (the pitch change in semitones / 12) = the new tempo
+	 * The new tempo = the old tempo * 2 ^ (the pitch change in semitones / 12)
 	 */
 
-	millisecondsPerBeat = 60000.0 / beatsPerMinute;
-
 	for (semitones = -2; semitones < 3; semitones++) {
-		printf("%3d\t   %2d\t%7.2f\n", beatsPerMinute, semitones, beatsPerMinute * pow(2, semitones / 12.0));
+		printf("%3d\t%+3d\t%7.3f\n", beatsPerMinute, semitones, beatsPerMinute * pow(2, semitones / 12.0));
 	}
 
 	return;
@@ -25,14 +22,14 @@ int main(int argc, char *argv[]) {
 	int beatsPerMinute;
 
 	printf("BPM\tSts\tBPM\n");
-	printf("===\t===\t===\n\n");
+	printf("===\t===\t=======\n\n");
 
 	if (argc == 1) {
 		for (beatsPerMinute = 80; beatsPerMinute < 145; beatsPerMinute += 5) {
-			beatsPerMinuteToSemitones(beatsPerMinute);
+			transposeBeatsPerMinute(beatsPerMinute);
 		}
 	} else {
-		beatsPerMinuteToSemitones(atoi(argv[1]));
+		transposeBeatsPerMinute(atoi(argv[1]));
 	}
 
 	return 0;
